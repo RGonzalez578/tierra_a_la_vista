19081a503b1529a945c735682a912113521b5c13
WLARA230700 <willy.lara@ucr.ac.cr>
2021-11-05 23:08:35 -0600

    Movimiento del jugador y flote
    Se logra el movimiento del jugador aplicando diferentes fuerzas, así como el movimiento de que se encuentra a flote.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Private 
+    private Vector3 posicionInicial;
+    private float movimientoTransversal, movimientoLongitudinal, jump;
+    private Rigidbody rigidBody;
+    private Vector3 anguloRotacion;
+
+    // Public
+    public float velocidad = 1.5f;
+    public float profundidadInicial = 1f;
+    public float desplazamiento = 3f;
+    public Vector3 desplazamientoCamara;
+    public Camera mainCamera;
+
+    private void FixedUpdate()
     {
-        
+        if (transform.position.y < 9)
+        {
+            float multiplicador = Mathf.Clamp01(transform.position.y / profundidadInicial) * desplazamiento;
+            rigidBody.AddForce(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * multiplicador, 0f), ForceMode.Acceleration);
+        }
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        rigidBody = this.GetComponent<Rigidbody>();
+        posicionInicial = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+    }
+
+    
     void Update()
     {
-        
+        movimientoTransversal = Input.GetAxis("Horizontal");
+        movimientoLongitudinal = Input.GetAxis("Vertical");
+        jump = Input.GetAxis("Jump");
+
+        if (movimientoLongitudinal != 0 || movimientoTransversal != 0)
+        {
+            rigidBody.AddRelativeForce(Vector3.forward * -movimientoLongitudinal * velocidad);
+            //transform.Translate(Vector3.back * movimientoLongitudinal * velocidad * Time.deltaTime);
+
+            anguloRotacion = (new Vector3(0, movimientoTransversal, 0)) * velocidad;
+            Quaternion deltaRotation = Quaternion.Euler(anguloRotacion * Time.fixedDeltaTime);
+            rigidBody.MoveRotation(rigidBody.rotation * deltaRotation);
+        }
+        mainCamera.transform.position = (transform.position + desplazamientoCamara);
     }
 }
