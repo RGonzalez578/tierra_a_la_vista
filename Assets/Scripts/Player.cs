05101623c5289e7311c6daa68c3d7b1f4b250d4f
WLARA230700 <willy.lara@ucr.ac.cr>
2021-11-16 21:50:58 -0600

    Disparos completado
    También se hizo la animación de pérdida por contacto con el enemigo.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,7 +15,9 @@ public class Player : MonoBehaviour
     private float contadorMinutos = 0;
     private bool puertosHabilitados = true;
     private float contColdownPerdida = 0;
-    private int enemigosEliminados = 1;
+    private int enemigosEliminados = 0;
+    private float fuerzaMunicion = 20f;
+    private float cooldown = 0;
 
     // Public
     public float velocidad = 1.5f;
@@ -25,11 +27,16 @@ public class Player : MonoBehaviour
     public float limiteSegundos = 59f;
     public float limiteMinutos = 5f;
     public int oro = 50;
+    public GameObject municionJugador;
+    public GameObject containerMunicionAdelante;
+    public GameObject containerMunicionIzq;
+    public GameObject containerMunicionDer;
 
 
     public Text lblOro;
     public Text lblTiempo;
     public Text txtMensajes;
+    public Text txtEnemigosEliminados;
 
     public Muelle[] muelles;    
 
@@ -38,6 +45,7 @@ public class Player : MonoBehaviour
         rigidBody = this.GetComponent<Rigidbody>();
         posicionInicial = new Vector3(transform.position.x, transform.position.y, transform.position.z);
         lblOro.text = oro.ToString();
+        txtEnemigosEliminados.text = enemigosEliminados.ToString();
         contColdownPerdida = 3f;
 
 
@@ -112,6 +120,27 @@ public class Player : MonoBehaviour
                 mainCamera.transform.position = (transform.position + desplazamientoCamara);
             }
         }
+        else
+        {
+            anguloRotacion = (new Vector3(UnityEngine.Random.Range(-1, 1), 0, UnityEngine.Random.Range(-1, 1))) * velocidad;
+            Quaternion deltaRotation = Quaternion.Euler(anguloRotacion * Time.fixedDeltaTime);
+            rigidBody.MoveRotation(rigidBody.rotation * deltaRotation);
+        }
+
+        if (cooldown > 0)
+        {
+            cooldown -= Time.deltaTime;
+        }
+        if (cooldown <= 0)
+        {
+            cooldown = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.K) && cooldown <= 0)
+        {
+            disparar();
+            cooldown = 3;
+        }
 
     }
 
@@ -151,6 +180,12 @@ public class Player : MonoBehaviour
         lblOro.text = oro.ToString();
     }
 
+    public void sumarEnemigosEliminados()
+    {
+        enemigosEliminados++;
+        txtEnemigosEliminados.text = enemigosEliminados.ToString();
+    }
+
     public int calcularPuntaje(bool win)
     {
         int puntajeJugador = 0;
@@ -163,5 +198,21 @@ public class Player : MonoBehaviour
             puntajeJugador = enemigosEliminados * 100;
         }
         return puntajeJugador;
+    }
+    public void disparar()
+    {
+        var municionAdelante = GameObject.Instantiate(municionJugador, containerMunicionAdelante.transform.position, containerMunicionAdelante.transform.rotation);
+        var municionizq = GameObject.Instantiate(municionJugador, containerMunicionIzq.transform.position, containerMunicionIzq.transform.rotation);
+        var municionDer = GameObject.Instantiate(municionJugador, containerMunicionDer.transform.position, containerMunicionDer.transform.rotation);
+
+        municionAdelante.GetComponent<Rigidbody>().AddRelativeForce(Vector3.back * fuerzaMunicion, ForceMode.Impulse);
+        municionAdelante.GetComponent<Rigidbody>().AddRelativeForce(Vector3.up * (fuerzaMunicion/5), ForceMode.Impulse);
+
+        municionizq.GetComponent<Rigidbody>().AddRelativeForce(Vector3.right * fuerzaMunicion, ForceMode.Impulse);
+        municionizq.GetComponent<Rigidbody>().AddRelativeForce(Vector3.up * (fuerzaMunicion / 5), ForceMode.Impulse);
+
+        municionDer.GetComponent<Rigidbody>().AddRelativeForce(Vector3.left * fuerzaMunicion, ForceMode.Impulse);
+        municionDer.GetComponent<Rigidbody>().AddRelativeForce(Vector3.up * (fuerzaMunicion / 5), ForceMode.Impulse);
+
     }
 }
